d0c703bb6f46276b85527b83a80c825ac33606a3
Durgesh <durgeshprabhugaonkar9@gmail.com>
2025-11-10 13:33:31 +0530

    UI implementation

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -6,10 +6,14 @@ import Link from "next/link"
 import { useEffect, useState } from "react"
 import { useDebounceValue } from 'usehooks-ts'
 import { toast } from "sonner"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import { signUpSchema } from "@/schemas/signUpSchema"
 import axios,{AxiosError} from 'axios'
 import { ApiResponse } from "@/types/ApiResponse"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react"
 
 const page = () => {
 
@@ -52,12 +56,108 @@ const page = () => {
     checkUsernameUnique()
   }, [debouncedUsername])
 
-  const onSubmit = async (data) => {
-
+  const onSubmit = async (data:z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true)
+    try {
+      const response = await axios.post<ApiResponse>('/api/sign-up', data)
+      toast.success('Success', {
+        description: response.data.message,
+      })
+      router.push(`/verify/${username}`)
+      setIsSubmitting(false)
+    } catch (error) {
+        console.error("Error in SignUp of User",error)
+        const axiosError = error as AxiosError<ApiResponse>;
+        let errorMessage = axiosError.response?.data.message
+        toast.error('Signup Failed', {
+          description: errorMessage,
+        })
+        setIsSubmitting(false)
+    }
   }
 
   return (
-    <div>Page</div>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Mystery Message 
+          </h1>
+          <p className="mb-4">Sign up to start your annoymous adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6">
+          <FormField
+            name="username"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="username" 
+                  {...field}
+                  onChange={(e)=> {
+                    field.onChange(e)
+                    setUsername(e.target.value)
+                  }} 
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="email"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="email" 
+                  {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="password"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="password" 
+                  {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" disabled={isSubmitting}>
+            {
+              isSubmitting ? (
+                <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin"/> Please wair
+                </>
+              ) : ('Signup')
+            }
+          </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+            <p>
+              Already a member?{' '}
+              <Link href="/sign-in" className="text-blue-600 hover:text-blue-800">
+              Sign in
+              </Link>
+            </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
